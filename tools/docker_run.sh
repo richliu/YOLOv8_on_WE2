9925f3d7342b349092a0d029b4dbcfca7afd682a
Ubuntu <himax@himaxbuild.cezdjruuiviutjgnahwe14ehih.ix.internal.cloudapp.net>
2024-08-07 05:20:41 +0000

    fixed:  xterm need no root user feat: add docker share folder

--- tools/docker_run.sh
+++ tools/docker_run.sh
@@ -9,8 +9,11 @@ DOCKER_IMAGE="himax/fvp"
 
 ### check container is ready
 echo "create $container_name container"
-#docker run --rm -it himax/fvp bash bash 
-docker run --rm --name himax_fvp -idt himax/fvp bash
+#docker run --rm -it --name himax_fvp himax/fvp bash
+docker run --rm --name himax_fvp -idt \ 
+    -v /home/himax/YOLOv8_on_WE2/docker/runtime:/home/ubuntu/YOLOv8_on_WE2/ml-embedded-evaluation-kit/runtime \ 
+    himax/fvp bash
+
 docker run --rm -idt -e DISPLAY=:10.0 himax/fvp bash
 #docker run --rm -it -v /labs/corstone/docker/runtime:/home/ml-embedded-evaluation-kit/runtime \
 #		-e DISPLAY=:0 \
